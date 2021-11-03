bfbcbfe42cc1cdfe5a9b5ecc8112842e3a1ac1f5
rllynotfox <92391169+RllyNotODKM@users.noreply.github.com>
2021-11-03 18:01:19 +0300

    dfgetyrtg
    erterter

--- code.js
+++ code.js
@@ -1,10 +1,3 @@
-var cnv = document.querySelector('canvas')
-var ctx = cnv.getContext('2d')
-
-cnv.width = window.innerWidth;
-cnv.height = window.innerHeight;
-
-cnv.addEventListener('pointerdown', () => {
-    ctx.fillStyle = '#'+Math.floor(Math.random() * 16777215).toString(16)
-    ctx.fillRect(0,0,cnv.width, cnv.height)
+document.addEventListener('pointerup', () => {
+    document.querySelector('body').style.background = '#' + Math.random().toString(16).substr(-6)
 })
